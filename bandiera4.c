53d5bb68953f69cfcfc0beede89fb254b009036c
rommelandrea <rommel@mefisto>
2011-10-25 11:44:38 +0200

    inizializzato progetto

---
+++ bandiera4.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   bandiera4.c
+ * Author: rommel
+ *
+ * Created on October 25, 2011, 11:44 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
